63eed2def91b2ae836fb16c68b61f496fa21371c
vash512 <phyrox.vash512@gmail.com>
2015-03-17 15:49:23 -0600

    settings sin summernote y djangocms-admin-style

--- backend/terminus/terminus/settings.py
+++ backend/terminus/terminus/settings.py
@@ -17,14 +17,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'djangocms_admin_style',
+    #'djangocms_admin_style',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_summernote', 
+    #'django_summernote', 
     #'home',
     'terminos',
     #'usuarios',
