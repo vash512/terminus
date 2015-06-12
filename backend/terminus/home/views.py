d109c98e5f158e4c07c07e58be51c570920e7986
vash512 <phyrox.vash512@gmail.com>
2015-06-12 18:36:11 -0500

    menu y login corregido

--- backend/terminus/home/views.py
+++ backend/terminus/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.http import HttpResponseRedirect as redir
+from django.http import HttpResponseRedirect
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate, logout
@@ -60,8 +60,35 @@ def corpus(request):
 
 def log_in(request):
     'Pagina de Login(Inicio de sesion) de Ontomex'
-    return render_to_response('home/login.html',
+    usuario=request.user
+    if usuario.is_anonymous():
+        if request.method=='POST':
+            #solicitud de login por post
+            if True:
+                usuariof=request.POST.get('username','')
+                clavef=request.POST.get('password','')
+                acceso=authenticate(username=usuariof,password=clavef)
+                if acceso is not None:
+                    if acceso.is_active:
+                        login(request,acceso)
+                        #Pantalla del Perfil
+                        return HttpResponseRedirect('/')
+                    else:
+                        error="Posiblemente tu usuario este baneado o desactivado, comunicate con el administrador"
+                else:
+                    error="El usuario no existe, verifique que este bien escrito"
+            else:
+                error="Datos invalidos en el formulario"
+            ctx={'error':error}
+            return render_to_response('home/login.html', ctx,
                           context_instance=RequestContext(request))
+        else:
+            #Login
+            return render_to_response('home/login.html',
+                          context_instance=RequestContext(request))
+    else:
+        return HttpResponseRedirect('/')
+
 
 def terminos(request):
     return render_to_response('terminos/terminos.html',
